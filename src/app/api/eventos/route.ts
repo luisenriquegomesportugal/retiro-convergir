027b79a1147136a6ab5f6b622da1ea222603cc08
Luis Enrique Gomes Portugal <luisenriquegomesportugal@gmail.com>
2025-01-04 16:06:11 -0300

    remove inscritos key from eventos

--- src/app/api/eventos/route.ts
+++ src/app/api/eventos/route.ts
@@ -9,5 +9,7 @@ export async function GET() {
 
     const eventos = Object.values<EventoType>(snapshotEventos.val());
 
+    eventos.forEach(evento => delete evento.inscricoes)
+
     return Response.json({ eventos })
 }
