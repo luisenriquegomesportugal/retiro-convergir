c6dc742c2e65b03eb3326fa4b7d32eb4a148339e
Luis Enrique Gomes Portugal <luisenriquegomesportugal@gmail.com>
2026-02-23 16:15:39 -0300

    Uncomment ValoresTotaisCard component in page.tsx

--- src/app/eventos/[eventoId]/page.tsx
+++ src/app/eventos/[eventoId]/page.tsx
@@ -39,7 +39,7 @@ export default async function EventoPage({ params }: Props) {
     <div className="flex flex-col sm:flex-row lg:flex-col gap-4 w-full md:max-w-[200px] lg:max-w-md">
       <PagamentoParcelasCard inscricoes={inscricoes.filter(i => !i.desativado)} />
       <ValoresInscricoesDiaCard inscricoes={inscricoes} />
-      {/* <ValoresTotaisCard inscricoes={inscricoes} /> */}
+      <ValoresTotaisCard inscricoes={inscricoes} />
     </div>
   </div>
 }
